6d160235104207f8db1aa304e872507bac737633
Muhammad Muaz Hassan <111386953+muazDS@users.noreply.github.com>
2023-11-23 22:36:31 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,22 @@ provider "google" {
   region      = var.region
 }
 
+variable "credentials_path" {
+  description = "Path to the GCP service account credentials file"
+}
+
+variable "project_id" {
+  description = "Google Cloud Platform Project ID"
+}
+
+variable "region" {
+  description = "GCP region for the resources"
+}
+
+variable "bucket_name" {
+  description = "Name for the Google Cloud Storage bucket"
+}
+
 resource "google_storage_bucket" "example_bucket" {
   name          = var.bucket_name
   location      = var.region
