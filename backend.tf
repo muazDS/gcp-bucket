c30bea99fd86a274421c289f52e69e551334db60
Muhammad Muaz Hassan <111386953+muazDS@users.noreply.github.com>
2023-11-24 19:21:11 +0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,5 +2,6 @@ terraform {
   backend "gcs" {
     bucket         = "state-files-bucket-git"
     prefix         = "terraform/state"
+    credentials    = "./neural-water-404520-2b5b17d0cb26.json"
   }
 }
